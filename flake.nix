efc6e0d68630205b7dded8fb549f8b3e6cf964d5
murmeldin <mail@marekkrug.de>
2024-07-25 21:00:50 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      hmLib = home-manager.lib;
+    in
+    {
+      nixosConfigurations.miracunix = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+
+      homeConfigurations = {
+        murmeldin = hmLib.homeManagerConfiguration {
+          pkgs = pkgs;
+          modules = [
+            ./home.nix
+          ];
+        };
+      };
+    };
+}
